f616b75cc79166e45c61a2e3088f39d227a7ba25
HeltonCarlos <heltonbritopa@gmail.com>
2021-04-16 12:19:05 -0300

    produtos README
    PRODUTOS

---
+++ javascript/api.js
@@ -0,0 +1,58 @@
+const produtos =[
+    {
+        id:01,
+        nome:'SKATE STREET OWL',
+        descricao:'Shape de skate Street nacional bastante dinâmico, encaixa em várias configurações de skate,Possui 7 folhas de Marfim que garante um ótimo equilíbrio entre absorção de impacto e leveza. Os shapes de skates OWL são fabricados no Brasil com extração controlada e certificada.',
+        preco:91.90,
+        img:'/img/produtos/SKATE STREET OWL SPORTS.png'
+    },
+    {
+        id:02,
+        nome:'TraxartSkate Traxart X-Pro Skeleton DW-233',
+        descricao:'O Skate Traxart X-PRO é um skate montado com as melhores peças do mercado; por isso é o skate top de linha da Traxart.',
+        preco:180.90,
+        img:'/img/produtos/TraxartSkate Traxart X-Pro Skeleton DW-233.png'
+    },
+    {
+        id:03,
+        nome:' Kit Skate Montado Iniciante Ratus Flame',
+        descricao:'Kit Skate Montado Iniciante Ratus Flame, Shape Ratus 8.0", Truck 139MM This Way, Roda 53MM This Way, Rolamento Kolami, Chave This Way, Parafuso John, Lixa John.',
+        preco:349.99,
+        img:'/img/produtos/Kit Skate Montado Iniciante Ratus Flame.png'
+    },
+    {
+        id:04,
+        nome:'Skate Traxart X-Pro Black and White Skull DW-243',
+        descricao:'O Skate Traxart X-PRO é um skate montado com as melhores peças do mercado; por isso é o skate top de linha da Traxart.',
+        preco:190.90,
+        img:'/img/produtos/Skate Traxart X-Pro Black and White Skull DW-243.png'
+    },
+    {
+        id:05,
+        nome:'Patins Logo Black Abec 5 Mindblower',
+        descricao:'LOGO BLACK - Fila Skates Patins Tradicional Quad Fila Skates LOGO, Preto e branco, com base e truck de composite , rodas 54mmx32mm/80A e rolamentos.',
+        preco:699.99,
+        img:'/img/produtos/LOGO BLACK.png'
+    },
+    {
+        id:06,
+        nome:'Skate Longboard',
+        descricao:'O Skate Longboard Speed da OWL é ideal para quem gosta de sentir a velocidade em seus pés, descer ladeiras e percorrer longas distâncias, conta com rodas fundidas (derramadas) do nosso modelo OWL SUN, rodas que tem ótimo grip, não recomendadas para lentidão ou moscação. Por ser bem estável é um longboard fácil para aprender e se equilibrar.',
+        preco:255.00,
+        img:'/img/produtos/Skate Longboard.png'
+    },
+    {
+        id:07,
+        nome:'Skate Barbie',
+        descricao:'Detalhes do produtoMarca FunModelo 7619-1Referência Skate BarbieIdade recomendada A partir dos 08 anosGênero FemininoDesenvolvimento pedagógico Agilidade,Coordenação motora,Tato,audição e visãoGarantia do fabricante 03 meses.',
+        preco:321.90,
+        img:'/img/produtos/Skate_com_Acessorios_Barbie.png'
+    },
+    {
+        id:08,
+        nome:'SKATE JURASSIC WORLD',
+        descricao:'Skate Infantil Jurassic World Portal™ foi pensado para as crianças que estão sempre em busca de novas formas de se divertir e brincar. Com Shape Maple 31, trucks feitos em alumínio, rolamentos ABEC 5 além de contar com uma linda estampa de Jurassic World™. Feito em material resistente, o Skate é um ótimo presente para as crianças que gostam de esportes radicais sem deixar de lado a segurança.',
+        preco:190.99,
+        img:'/img/produtos/skateboard_j_w.png'  
+    }
+]
